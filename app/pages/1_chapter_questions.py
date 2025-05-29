9e23e323cb9efb327727b0adc77169df0fb863ed
DavidePanza <davide.panza@gmail.com>
2025-05-29 16:09:29 +0200

    fix image display

--- app/pages/1_chapter_questions.py
+++ app/pages/1_chapter_questions.py
@@ -73,21 +73,7 @@ else:
 
 
 
-for idx, question_item in enumerate(st.session_state['questions_json']):
-    question_text = question_item['question']
-    answer_text = question_item['answer']
 
-    col1, col2 = st.columns([0.9, 0.1])
-    
-    with col1:
-        st.html(f"<p style='font-size:20px; margin:0;'>{idx+1}. {question_text}</p>")
-        col1_ = st.columns([1, 4])[0]
-        with col1_:
-            with st.expander("💡 Show Answer"):
-                st.write(answer_text)
-
-    with col2:
-        selected = st.checkbox("📌", key=f"select_{idx}", value=True)
 
 # breaks(2)
 
